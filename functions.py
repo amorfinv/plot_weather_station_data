43cdeb9ae215cf4db44ae0d17d3893e85cb965bd
Akshay Patil <patil.akshay11235@gmail.com>
2023-11-02 20:59:27 +0100

    Adding windrose script

--- functions.py
+++ functions.py
@@ -2,6 +2,7 @@ import numpy as np
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
+import matplotlib.pyplot as plt
 #
 # Function to download files from the weather repository
 #
@@ -42,3 +43,32 @@ def list_csv_files_at_url(url):
     csv_files = [file_url for file_url in file_urls if file_url.lower().endswith('.csv')]
     
     return csv_files
+#
+# Set default plotting size
+#
+def fixPlot(thickness=1.5, fontsize=20, markersize=8, labelsize=15, texuse=False, tickSize = 15):
+    '''
+        This plot sets the default plot parameters
+    INPUT
+        thickness:      [float] Default thickness of the axes lines
+        fontsize:       [integer] Default fontsize of the axes labels
+        markersize:     [integer] Default markersize
+        labelsize:      [integer] Default label size
+    OUTPUT
+        None
+    '''
+    # Set the thickness of plot axes
+    plt.rcParams['axes.linewidth'] = thickness    
+    # Set the default fontsize
+    plt.rcParams['font.size'] = fontsize    
+    # Set the default markersize
+    plt.rcParams['lines.markersize'] = markersize    
+    # Set the axes label size
+    plt.rcParams['axes.labelsize'] = labelsize
+    # Enable LaTeX rendering
+    plt.rcParams['text.usetex'] = texuse
+    # Tick size
+    plt.rcParams['xtick.major.size'] = tickSize
+    plt.rcParams['ytick.major.size'] = tickSize
+    plt.rcParams['xtick.direction'] = 'in'
+    plt.rcParams['ytick.direction'] = 'in'
---
+++ windRoseAvgWind.py
@@ -0,0 +1,62 @@
+# Mostly copied from Andres' script #
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from functions import fixPlot
+
+num_bins = 64
+figx, figy = 10,8
+range_to_plot = ['2022-01-01','2022-12-31']
+
+# read in the column names,  remove new line characters, and empty spaces
+with open('data/fields.txt', 'r') as file:
+   columns = file.readlines()
+columns = [column.rstrip('\n') for column in columns]
+columns = [column.split(" ")[-1] for column in columns]
+
+# csv read configurations
+csv_args = { 
+    'on_bad_lines': 'skip',
+    'names': columns, 
+    'na_values': ['NAN'],
+    'dtype': {'Albedo': float},
+    'comment': '^'
+}
+# Read the database
+stations = ['Oost', 'Ommoord', 'Bolnes', 'SpaansePolder', 'Ridderkerk', 'Heijplaat', 'Delfshaven']
+#stations = ['Delfshaven']  # Uncomment to debug
+fixPlot(thickness=1.5, fontsize=25, markersize=8, labelsize=20, texuse=True, tickSize = 15)
+for iternum in range(len(stations)):
+    filein='data/'+stations[iternum]+'.csv'
+    df = pd.read_csv(filein,**csv_args)
+    # convert datetime to datetime object
+    df['DateTime'] = pd.to_datetime(df['DateTime'], errors='coerce')
+    # Define the desired time range
+    start_date = pd.to_datetime(range_to_plot[0])
+    end_date = pd.to_datetime(range_to_plot[1])
+    # Filter the data
+    df_filtered = df[(df['DateTime'] >= start_date) & (df['DateTime'] <= end_date)]
+
+    # PLOTTING
+
+    # Calculate bin edges and centers for wind directions
+    bin_edges = np.linspace(0, 360, num_bins + 1, endpoint=True)
+    bin_centers = 0.5 * (bin_edges[1:] + bin_edges[:-1])
+    # Compute the histogram
+    windHist, _ = np.histogram(df_filtered['WindDir_{Avg}'],bins=bin_edges)
+    fig = plt.figure(iternum+1,figsize=(figx,figy))
+    ax = plt.subplot(1,1,1,polar=True)
+    # Plot windrose histogram
+    ax.bar(np.radians(bin_centers), windHist, width=np.radians(360 / num_bins),  
+        align="center", color="red", edgecolor="black")
+    # Customize the windrose plot
+    ax.tick_params(axis='both', pad=20)
+    ax.set_theta_offset(np.radians(90))  
+    ax.set_theta_direction(-1)  
+    ax.set_rlabel_position(0)  
+    ax.set_rticks([])  
+    
+    ax.set_title(stations[iternum], va='bottom')
+plt.show()
+
